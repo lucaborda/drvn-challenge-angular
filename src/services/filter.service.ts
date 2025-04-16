c8b21d435cb57d89cc7cf19a23aa58209f2ab4a1
lucaborda <lucaborda11@gmail.com>
2025-04-16 11:59:13 -0300

    filter service

---
+++ src/services/filter.service.ts
@@ -0,0 +1,44 @@
+// services/filter.service.ts
+import { Injectable, signal, computed } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+type CategoryResponse = {
+  name: string;
+  slug: string;
+  url: string;
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FilterService {
+  private _selectedCategory = signal<string | null>(null);
+  private _availableCategories = signal<string[]>([]);
+
+  selectedCategory = this._selectedCategory.asReadonly();
+  availableCategories = this._availableCategories.asReadonly();
+
+  hasFilter = computed(() => this.selectedCategory() !== null);
+
+  constructor(private http: HttpClient) {
+    this.loadCategories();
+  }
+
+  loadCategories() {
+    this.http
+      .get<CategoryResponse[]>(`https://dummyjson.com/products/categories`)
+      .subscribe({
+        next: (categories) =>
+          this._availableCategories.set(categories.map((c) => c.slug)),
+        error: (err) => console.error('Error loading categories', err),
+      });
+  }
+
+  setCategory(category: string | null) {
+    this._selectedCategory.set(category);
+  }
+
+  clearFilter() {
+    this._selectedCategory.set(null);
+  }
+}
